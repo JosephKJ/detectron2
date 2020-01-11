531aa73e071e1360d49f4c90091dfc64e8b2e0b7
Joseph <josephkj20@gmail.com>
2020-01-11 11:17:46 +0400

    Distillation_base

--- detectron2/modeling/distillation_loss.py
+++ detectron2/modeling/distillation_loss.py
@@ -1,16 +1,34 @@
 import torch
+import torch.nn.functional as F
 
 
 def rpn_loss(pred_objectness_logits, pred_anchor_deltas, prev_pred_objectness_logits, prev_pred_anchor_deltas):
-    loss = 0
-    return {"loss_dist_rpn": torch.tensor(loss)}
+    loss = logit_distillation(pred_objectness_logits[0], prev_pred_objectness_logits[0])
+    loss += anchor_delta_distillation(pred_anchor_deltas[0], prev_pred_anchor_deltas[0])
+    return {"loss_dist_rpn": loss}
 
 
 def backbone_loss(features, prev_features):
-    loss = 0
-    return {"loss_dist_backbone": torch.tensor(loss)}
+    loss = feature_distillation(features['res4'], prev_features['res4'])
+    return {"loss_dist_backbone": loss}
 
 
 def roi_head_loss(pred_class_logits, pred_proposal_deltas, prev_pred_class_logits, prev_pred_proposal_deltas):
-    loss = 0
-    return {"loss_dist_roi_head": torch.tensor(loss)}
+    loss = logit_distillation(pred_class_logits, prev_pred_class_logits)
+    loss += anchor_delta_distillation(pred_proposal_deltas, prev_pred_proposal_deltas)
+    return {"loss_dist_roi_head": loss}
+
+
+def logit_distillation(current_logits, prev_logits, T=2.0):
+    p = F.log_softmax(current_logits / T, dim=1)
+    q = F.softmax(prev_logits / T, dim=1)
+    kl_div = torch.sum(F.kl_div(p, q, reduce=False).clamp(min=0.0) * (T**2)) / current_logits.shape[0]
+    return kl_div
+
+
+def anchor_delta_distillation(current_delta, prev_delta):
+    return F.mse_loss(current_delta, prev_delta)
+
+
+def feature_distillation(features, prev_features):
+    return F.mse_loss(features, prev_features)
